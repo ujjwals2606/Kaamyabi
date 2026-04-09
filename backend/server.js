c4b5ec8f1addf47c8be6a2b739943a992f2aad99
ujjwals2606 <smartujjwalsingh47@gmail.com>
2026-04-09 16:59:14 +0530

    Updated leadership section with co-founder

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ connectDB();
 // ✅ CORS
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://kaamyabi00.onrender.com/",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
